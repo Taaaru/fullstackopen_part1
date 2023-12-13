61b0181e94dbb7fe8f5a7bc99f10daa733305366
Andy <112675389+Taaaru@users.noreply.github.com>
2023-12-13 18:25:34 +0700

    exercise345

---
+++ exercise345/src/App.jsx
@@ -0,0 +1,70 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course.name}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      {props.course.parts.map(part => (
+        <Part key={part.name} name={part.name} exercises={part.exercises} />
+      ))}
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.name} {props.exercises}
+      </p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  let sum = 0
+  props.course.parts.forEach(part => {
+    sum += part.exercises
+  })
+
+  return (
+    <>
+      <p>Number of exercises {sum}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+export default App
